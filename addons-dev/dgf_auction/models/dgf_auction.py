f350ea3bd018c63677949e03b9a5f509a4bf1de3
SerhiiZavalko <sergey.zavalko@gmail.com>
2023-03-09 22:39:22 +0200

    dgf_auction

--- addons-dev/dgf_auction/models/dgf_auction.py
+++ addons-dev/dgf_auction/models/dgf_auction.py
@@ -20,44 +20,44 @@ class DgfAuction(models.Model):
 
     name = fields.Char(index=True, compute='_compute_name', store=True, readonly=True)
     _cdu = fields.Selection(
-        [("1", "ЦБД-1"), ("2", "ЦБД-2"), ("3", "ЦБД-3")],
-        string="ЦБД",
+        [('1', 'ЦБД-1'), ('2', 'ЦБД-2'), ('3', 'ЦБД-3')],
+        string='ЦБД',
         required=True,
         copy=False,
-        default="3",
+        default='3',
     )
     _id = fields.Char(string='Ідентифікатор технічний', index=True)
-    datePublished = fields.Datetime(string='datePublished', help="Дата")
-    dateModified = fields.Datetime(string='dateModified', help="Дата")
-    auctionPeriodStartDate = fields.Datetime(string='auctionPeriodStartDate', help="Дата")
+    datePublished = fields.Datetime(string='datePublished', help='Дата')
+    dateModified = fields.Datetime(string='dateModified', help='Дата')
+    auctionPeriodStartDate = fields.Datetime(string='auctionPeriodStartDate', help='Дата')
     auctionId = fields.Char(string='auctionId')
     previousAuctionId = fields.Char()
     sellingMethod = fields.Char(string='sellingMethod', index=True)
     lotId = fields.Char(string='lotId', index=True)
     currency_id = fields.Many2one('res.currency', string='Валюта аукціону', default=lambda self: self.env.ref('base.UAH'))
     value_amount = fields.Float('value_amount', digits=(15, 2))
-    value_currency = fields.Char(related="currency_id.name", store=True)
+    value_currency = fields.Char(related='currency_id.name', store=True)
     value_valueAddedTaxIncluded = fields.Boolean()
     valuePeriod = fields.Float('valuePeriod', digits=(15, 2))
     leaseDuration = fields.Float('leaseDuration', digits=(15, 2))
     status = fields.Char(string='Статус', index=True)
     description = fields.Text('description')
     title = fields.Text('title')
-    auctionUrl = fields.Char(string="Гіперпосилання на аукціон", readonly=False)
+    auctionUrl = fields.Char(string='Гіперпосилання на аукціон', readonly=False)
     owner = fields.Char()
     accessDetails = fields.Text()
 
     guarantee_amount = fields.Float(digits=(15, 2))
-    guarantee_currency = fields.Char(related="currency_id.name", store=True)
+    guarantee_currency = fields.Char(related='currency_id.name', store=True)
     registrationFee_amount = fields.Float(digits=(15, 2))
     tenderAttempts = fields.Integer()
 
     # dgf_auction_lot_id = fields.Many2one('dgf.auction.lot', string='Організатор')
     partner_id = fields.Many2one('res.partner', string='Організатор')
     company_id = fields.Many2one('res.company', required=True, default=lambda self: self.env.company)
-    href = fields.Char(string="Гіперпосилання", compute='_compute_href', store=True, readonly=False)
-    active = fields.Boolean(string='Активно', default=True, help="Чи є запис активним чи архівованим.")
-    update_date = fields.Datetime(string='Оновлено через API', help="Дата")
+    href = fields.Char(string='Гіперпосилання', compute='_compute_href', store=True, readonly=False)
+    active = fields.Boolean(string='Активно', default=True, help='Чи є запис активним чи архівованим.')
+    update_date = fields.Datetime(string='Оновлено через API', help='Дата')
 
     notes = fields.Text('Примітки')
 
@@ -84,6 +84,59 @@ class DgfAuction(models.Model):
             dateModified = datetime.strptime(responce['dateModified'][:-1], '%Y-%m-%dT%H:%M:%S.%f') if responce['dateModified'] is not None else None
             datePublished = datetime.strptime(responce['datePublished'][:-1], '%Y-%m-%dT%H:%M:%S.%f') if responce['datePublished'] is not None else None
             auctionPeriodStartDate = datetime.strptime(responce['auctionPeriod']['startDate'][:-1], '%Y-%m-%dT%H:%M:%S.%f') if responce['auctionPeriod'] is not None else None
+            sellingEntityId = responce['sellingEntity']['identifier']['id']
+            sellingEntity = self.env['res.partner'].search([('vat', '=', sellingEntityId)])
+# requestDate = fields.Datetime.now()
+            if responce['_id']:
+                data = responce
+                # TODO: revise different logic for legal & individuals
+
+                # beginDate = fields.Date.to_date(
+                #     data['beginDate'][:-1]) if data['beginDate'] is not None else None
+
+                self.write({
+                    'update_date': datetime.utcnow().replace(microsecond=0),
+                    '_id': data['_id'],
+                    'description': data['description']['uk_UA'],
+                    'title': data['title']['uk_UA'],
+                    'sellingMethod': data['sellingMethod'],
+                    'dateModified': dateModified,
+                    'datePublished': datePublished,
+                    'auctionPeriodStartDate': auctionPeriodStartDate,
+                    'lotId': data['lotId'],
+                    'auctionId': data['auctionId'],
+                    'value_amount': data['value']['amount'],
+                    # 'auctionUrl': data['auctionUrl'] if data['auctionUrl'] is not None else None,
+                    'owner': data['owner'],
+                    'status': data['status'],
+                    'partner_id': sellingEntity,
+                    'notes': json.dumps(responce, ensure_ascii=False, indent=4, sort_keys=True).encode('utf8')
+                })
+            else:
+                self.write({
+                    # 'requestDate': dateModified,
+                    'status': data['message'],
+                })
+            self.env.cr.commit()  # commit every record
+            result = True
+        else:
+            result = False
+        time.sleep(1)
+        return result
+
+    def update_auction(self):
+        # TODO:
+        # review & refactor getpublicbypbnum()
+        # split publicbypbnum methods: common part & special parts
+        responce = self.env['prozorro.api']._update_auction_detail(
+            _id=self._id, description='Prozorro API')
+        if responce is not None:
+            # datetime.now().replace(microsecond=0)
+            dateModified = datetime.strptime(responce['dateModified'][:-1], '%Y-%m-%dT%H:%M:%S.%f') if responce['dateModified'] is not None else None
+            datePublished = datetime.strptime(responce['datePublished'][:-1], '%Y-%m-%dT%H:%M:%S.%f') if responce['datePublished'] is not None else None
+            auctionPeriodStartDate = datetime.strptime(responce['auctionPeriod']['startDate'][:-1], '%Y-%m-%dT%H:%M:%S.%f') if responce['auctionPeriod'] is not None else None
+            sellingEntityId = responce['sellingEntity']['identifier']['id']
+            sellingEntity = self.env['res.partner'].search([('vat', '=', sellingEntityId)])
             # requestDate = fields.Datetime.now()
             if responce['_id']:
                 data = responce
@@ -107,6 +160,7 @@ class DgfAuction(models.Model):
                     # 'auctionUrl': data['auctionUrl'] if data['auctionUrl'] is not None else None,
                     'owner': data['owner'],
                     'status': data['status'],
+                    'partner_id': sellingEntity,
                     'notes': json.dumps(responce, ensure_ascii=False, indent=4, sort_keys=True).encode('utf8')
                 })
             else:
@@ -118,46 +172,5 @@ class DgfAuction(models.Model):
             result = True
         else:
             result = False
-        time.sleep(3)
-        return result
-
-    def update_auction(self):
-        # TODO:
-        # review & refactor getpublicbypbnum()
-        # split publicbypbnum methods: common part & special parts
-        responce = self.env['prozorro.api']._update_auction(
-            id=self._id, description='Prozorro API')
-        if responce is not None:
-            dateModified = datetime.strptime(
-                responce['dateModified'][:-1], '%Y-%m-%dT%H:%M:%S.%f') if responce['dateModified'] is not None else None
-            # requestDate = fields.Datetime.now()
-            if responce['results']:
-                data = responce['results'][0]
-                # TODO: revise different logic for legal & individuals
-                creditors = data['creditors'][0]
-                creditors['role_id'] = 'creditors'
-                debtors = data['debtors'][0]
-                debtors['role_id'] = 'debtors'
-                beginDate = fields.Date.to_date(
-                    data['beginDate'][:-1]) if data['beginDate'] is not None else None
-
-                self.write({
-                    'vdID': data['vdID'],
-                    'mi_wfStateWithError': data['mi_wfStateWithError'],
-                    'beginDate': beginDate,
-                    'dateModified': dateModified,
-                    # 'state': data['mi_wfStateWithError'],
-                    'status': data['status'],
-                    'notes': responce
-                })
-            else:
-                self.write({
-                    'requestDate': dateModified,
-                    'mi_wfStateWithError': 'Записів не знайдено',
-                })
-            self.env.cr.commit()  # commit every record
-            result = True
-        else:
-            result = False
-        time.sleep(3)
+        # time.sleep(3)
         return result
--- addons-dev/dgf_auction/models/prozorro_api.py
+++ addons-dev/dgf_auction/models/prozorro_api.py
@@ -3,10 +3,28 @@
 from odoo import api, models, exceptions, _
 from ..tools import http_tools
 
+# DEFAULT_ENDPOINT = {
+#     '1': {
+#         'base_path': 'https://public.api.ea.openprocurement.org/api/2/auctions/',
+#         'search_path': '',
+#         'get_path': '',
+#     },
+#     '2': {
+#         'base_path': 'https://public.api.ea2.openprocurement.net/api/2/auctions/',
+#         'search_path': '',
+#         'get_path': '',
+#     },
+#     '3': {
+#         'base_path': 'https://procedure.prozorro.sale/api/',
+#         'search_path': 'search/procedures/',
+#         'get_path': 'procedures/',
+#     }
+# }
+
 DEFAULT_ENDPOINT = 'https://procedure.prozorro.sale/api/'
 SEARCH_PATH = 'search/procedures/'
 GET_PATH = 'procedures/'
-# '/api/search/byDateModified/${dateModified}?limit=100'
+'/api/search/byDateModified/${dateModified}?limit=100'
 
 
 class ProzorroApi(models.AbstractModel):
@@ -15,41 +33,52 @@ class ProzorroApi(models.AbstractModel):
 
     @api.model
     def _contact_api(self, method='GET', api_method=None, payload=None, description=None):
-        endpoint = self.env['ir.config_parameter'].sudo().get_param('prozorro.endpoint', DEFAULT_ENDPOINT)
+        endpoint = self.env['ir.config_parameter'].sudo().get_param(
+            'prozorro.endpoint', DEFAULT_ENDPOINT)
         headers = {'Content-Type': 'application/json; charset=utf-8'}
         return http_tools.api_jsonrpc(self.env, endpoint + api_method, method=method, headers=headers, payload=payload, description=description)
 
     @api.model
     def _search_by(self, search_parameter=None, search_value=None, limit=None, description=None):
-        search_by = "{0}{1}/{2}".format(SEARCH_PATH, search_parameter, search_value) if limit is None else "{0}{1}/{2}?limit={3}".format(SEARCH_PATH, search_parameter, search_value, limit)
+        search_by = "{0}{1}/{2}".format(SEARCH_PATH, search_parameter, search_value) if limit is None else "{0}{1}/{2}?limit={3}".format(
+            SEARCH_PATH, search_parameter, search_value, limit)
         if search_parameter is not None and search_value is not None:
-            responce = self._contact_api(api_method=search_by, description=description)
+            responce = self._contact_api(
+                api_method=search_by, description=description)
             return responce
         else:
-            raise exceptions.UserError(_('Parameters {0}, {1} cannot be empty'.format('search_parameter', 'search_value')))
+            raise exceptions.UserError(_('Parameters {0}, {1} cannot be empty'.format(
+                'search_parameter', 'search_value')))
 
     @api.model
     def _byAuctionId(self, auction_id=None, description=None):
         search_by = 'byAuctionId'
         if auction_id is not None:
-            responce = self._search_by(search_parameter=search_by, search_value=auction_id, description=description)
+            responce = self._search_by(
+                search_parameter=search_by, search_value=auction_id, description=description)
             return responce
         else:
-            raise exceptions.UserError(_('Parameter {0} cannot be empty'.format(auction_id)))
+            raise exceptions.UserError(
+                _('Parameter {0} cannot be empty'.format(auction_id)))
 
     @api.model
     def _byDateModified(self, date_modified=None, limit=None, description=None):
         search_by = 'byDateModified'
         if date_modified is not None:
-            responce = self._search_by(search_parameter=search_by, search_value=date_modified, limit=limit, description=description)
+            responce = self._search_by(
+                search_parameter=search_by, search_value=date_modified, limit=limit, description=description)
             return responce
         else:
-            raise exceptions.UserError(_('Parameter {0} cannot be empty'.format('date_modified')))
+            raise exceptions.UserError(
+                _('Parameter {0} cannot be empty'.format('date_modified')))
 
     @api.model
-    def _update_auction(self, auction_id=None, description=None):
-        if auction_id is not None:
-            responce = self._contact_api(api_method=auction_id, description=description)
+    def _update_auction_detail(self, _id=None, description=None):
+        if _id is not None:
+            api_method = GET_PATH + _id
+            responce = self._contact_api(
+                api_method=api_method, description=description)
             return responce
         else:
-            raise exceptions.UserError(_('Parameter {0} cannot be empty'.format(auction_id)))
+            raise exceptions.UserError(
+                _('Parameter {0} cannot be empty'.format(_id)))
