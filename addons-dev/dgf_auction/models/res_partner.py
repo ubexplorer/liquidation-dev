74d43337914fec1a0efd6710e2e95cace5b3ca3a
SerhiiZavalko <sergey.zavalko@gmail.com>
2023-03-19 20:45:29 +0200

    dgf_auction

--- addons-dev/dgf_auction/models/res_partner.py
+++ addons-dev/dgf_auction/models/res_partner.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from datetime import datetime
+import time
+import json
 from odoo import models, fields
 
 
@@ -12,6 +15,17 @@ class Partner(models.Model):
                                   inverse_name='partner_id')
     auction_count = fields.Integer(
         string="Кількість аукціонів", compute='_compute_auction_count', store=False)
+
+    def search_byAuctionOrganizer(self):
+        self.ensure_one()
+        organizer_id = self.vat
+        date_now = datetime.strftime(datetime.now(), '%Y-%m-%dT%H:%M:%S')
+        # search_date = date_modified or date_now
+        responce = self.env['dgf.auction'].search_byAuctionOrganizer(organizer_id=organizer_id, date_modified=date_now)
+        if responce is not None:
+            return True
+        else:
+            return False
 
     def action_view_auctions(self):
         # print("self.display_name: {0}".format(self.display_name))
