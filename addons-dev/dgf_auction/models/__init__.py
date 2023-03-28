6ffb0116256c07f85e52cdcf1ae1db532614f531
SerhiiZavalko <sergey.zavalko@gmail.com>
2023-03-28 21:25:01 +0300

    dgf_auction

--- addons-dev/dgf_auction/models/__init__.py
+++ addons-dev/dgf_auction/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import dgf_auction
+from . import dgf_auction_category
 from . import prozorro_api
 from . import dgf_auction_lot
 from . import stat_classifier
---
+++ addons-dev/dgf_auction/models/dgf_auction_category.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from random import randint
+
+from odoo import api, fields, models, tools, SUPERUSER_ID, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class DgfAuctionCategory(models.Model):
+    _description = 'Категорії аукціонів'
+    _name = 'dgf.auction.category'
+    _order = 'name'
+    _parent_store = True
+
+    def _get_default_color(self):
+        return randint(1, 11)
+
+    name = fields.Char(string='Найменування', required=True)
+    code = fields.Char(string='Код', required=False)
+    _cdu = fields.Selection(
+        [('1', 'ЦБД-1'), ('2', 'ЦБД-2'), ('3', 'ЦБД-3')],
+        string='ЦБД',
+        required=False,
+        copy=False,
+        default='3',
+    )
+    color = fields.Integer(string='Color Index', default=_get_default_color)
+    parent_id = fields.Many2one('dgf.auction.category', string='Батьківська категорія', ondelete='cascade')  # index=True,
+    child_ids = fields.One2many('dgf.auction.category', 'parent_id', string='Дочірні категорії')
+    active = fields.Boolean(default=True, string='Активно', help="Чи є запис активним чи архівованим.")
+    parent_path = fields.Char()  # index=True
+    default_endpoint = fields.Char()
+    search_path = fields.Char()
+    get_path = fields.Char()
+    # document_ids = fields.Many2many('dgf.document', column1='category_id', column2='document_id', string='Документи')
+
+    @api.constrains('parent_id')
+    def _check_parent_id(self):
+        if not self._check_recursion():
+            raise ValidationError(_('You can not create recursive tags.'))
+
+    def name_get(self):
+        """ Return the categories' display name, including their direct
+            parent by default.
+
+            If ``context['partner_category_display']`` is ``'short'``, the short
+            version of the category name (without the direct parent) is used.
+            The default is the long version.
+        """
+        if self._context.get('partner_category_display') == 'short':
+            return super(DgfAuctionCategory, self).name_get()
+
+        res = []
+        for category in self:
+            names = []
+            current = category
+            while current:
+                names.append(current.name)
+                current = current.parent_id
+            res.append((category.id, ' / '.join(reversed(names))))
+        return res
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        if name:
+            # Be sure name_search is symetric to name_get
+            name = name.split(' / ')[-1]
+            args = [('name', operator, name)] + args
+        return self._search(args, limit=limit, access_rights_uid=name_get_uid)
