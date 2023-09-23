aece27050fcb504bcdd135c37c3e6b424778495d
SerhiiZavalko <sergey.zavalko@gmail.com>
2023-09-24 00:55:25 +0300

    dgf_auction

--- addons-dev/dgf_auction/models/__init__.py
+++ addons-dev/dgf_auction/models/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# from . import res_partner
 from . import dgf_auction
 from . import dgf_auction_category
 from . import prozorro_api
-# from . import dgf_auction_lot
-# from . import dgf_auction_award
+from . import dgf_auction_lot
+from . import dgf_auction_award
 from . import stat_classifier
 # from . import res_config_settings
 # from . import dgf_auction_lot_asset
+# from . import res_partner
